0241c2a4ed6c8ab3b4ded8a94b2bd67150df5dc5
Wade Holler <wholler@ittl.gov>
2017-03-21 22:01:21 -0400

    update with testing

---
+++ hello_test.go
@@ -0,0 +1,15 @@
+package main_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPrintIt(t *testing.T) {
+	if 'x' == 'x' {
+		fmt.Println("value is true")
+	} else {
+
+		fmt.Println("value is true")
+	}
+}
