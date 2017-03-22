021f74fd5950d51f5fd1ad66545b785877ded4f6
Wade Holler <wholler@ittl.gov>
2017-03-21 22:11:13 -0400

    update to remove github package

--- hello.go
+++ hello.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/wadeholler/string"
 )
 
 func main() {
-	fmt.Println("reverse = " + string.Reverse("Hello my world"))
 	fmt.Println("Hello my world")
 	fmt.Println("here is some more change testing")
 }
